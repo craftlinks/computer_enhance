e3673997404a6a32648a7b9104cd1ef45842b4d2
craftlinks <90352073+craftlinks@users.noreply.github.com>
2023-03-05 14:19:22 +0100

    Update main.zig

--- perfaware/part1/src/main.zig
+++ perfaware/part1/src/main.zig
@@ -5,13 +5,41 @@
 const std = @import("std");
 const fs = std.fs;
 
-const Masks = struct {
-    const OPERAND = 0b11111100;
-    const D = 0b00000010;
-    const W = 0b00000001;
-    const MOD = 0b11000000;
-    const REG = 0b00111000;
-    const RM = 0b00000111;
+const MOV = struct {
+    fn registerMemoryToFromMemory(buf: []u8, in_stream: anytype, asm_instruction_out: []u8) !void {
+        // d == 1 -> REG is destination operand
+        // d == 0 -> REG is source operand
+        const d = if ((buf[0] & 0b00000010) == 0b00000010) true else false;
+
+        // w == 1 -> word(16-bit) data;
+        // w == 0 -> byte(8-bit) data;
+        const w = if ((buf[0] & 0b00000001) == 0b00000001) true else false;
+
+        // fetch next byte in stream
+        _ = try in_stream.read(buf);
+
+        // mod == 11 -> register-to-register mode, only one allowed. Crash otherwise.
+        const mod = buf[0] & 0b11000000;
+        if (mod != 0b11000000) {
+            std.debug.print("MOD must be '0b11000000', found {b}", .{mod});
+            unreachable;
+        }
+
+        const reg = decode_register(buf[0] & 0b00111000, w);
+        const rm = decode_register(buf[0] & 0b00000111, w);
+
+        if (d) {
+            _ = try std.fmt.bufPrint(asm_instruction_out, "mov {s}, {s} \n", .{ reg, rm });
+        } else {
+            _ = try std.fmt.bufPrint(asm_instruction_out, "mov {s}, {s} \n", .{ rm, reg });
+        }
+    }
+
+    fn immediateToRegister(buf: []u8, in_stream: anytype, asm_instruction_out: []u8) !void {
+        _ = buf;
+        _ = in_stream;
+        _ = asm_instruction_out;
+    }
 };
 
 fn decode_register(byte: u8, w: bool) *const [2:0]u8 {
@@ -30,49 +58,11 @@ fn decode_register(byte: u8, w: bool) *const [2:0]u8 {
     return reg;
 }
 
-fn registerMemoryToFromMemory(buf: [1]u8, in_stream: anytype, decoded_string: []u8) ![]u8 {
-    // d == 1 -> REG is destination operand
-    // d == 0 -> REG is source operand
-    const d = if ((buf & 0b00000010) == 0b00000010) true else false;
-
-    // w == 1 -> word(16-bit) data;
-    // w == 0 -> byte(8-bit) data;
-    const w = if ((buf & 0b00000001) == 0b00000001) true else false;
-
-    // mod == 11 -> register-to-register mode, only one allowed. Crash otherwise.
-    const mod = buf & 0b11000000;
-    if (mod != 0b11000000) {
-        std.debug.print("MOD must be '0b11000000', found {d}", .{mod});
-        unreachable;
-    }
-
-    _ = in_stream.read(&buf);
-
-    const reg = decode_register(buf & 0b00111000, w);
-    const rm = decode_register(buf & 0b00000111, w);
-
-    if (d) {
-        const string = try std.fmt.bufPrint(&decoded_string, "mov {s}, {s} \n", .{ reg, rm });
-        return string;
-    } else {
-        const string = try std.fmt.allocPrint(&decoded_string, "mov {s}, {s} \n", .{ rm, reg });
-        return string;
-    }
-}
-
-fn immediateToRegister(buf: [1]u8) ![]u8 {
-    _ = buf;
-    return "";
-}
-
-fn decode(buf: [1]u8, in_stream: anytype) ![]u8 {
-    const decoded = switch (buf) {
-        (buf & 0b11111100) == 0b10001000 => registerMemoryToFromMemory(buf, in_stream),
-        (buf & 0b11110000) == 0b1011 => immediateToRegister(buf, in_stream),
-        else => unreachable,
-    };
-
-    return decoded;
+fn decode(buf: []u8, in_stream: anytype, asm_instruction_out: []u8) !void {
+    
+    if((buf[0] & 0b11111100) == 0b10001000) {try MOV.registerMemoryToFromMemory(buf, in_stream, asm_instruction_out);}
+    else if((buf[0] & 0b11110000) == 0b1011) {try MOV.immediateToRegister(buf, in_stream, asm_instruction_out);}
+    else unreachable;
 }
 
 pub fn main() !void {
@@ -82,12 +72,7 @@ pub fn main() !void {
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    // stdout
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    // Get file path from command line
+    // Get file path from command line.
     const args = try std.process.argsAlloc(allocator);
     if (args.len != 2) {
         std.debug.print("Required argument: path to Binary File\n", .{});
@@ -95,21 +80,27 @@ pub fn main() !void {
     }
     const binary_file_path = args[1];
 
+    // Prepare stdout
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
     // Open file and prepare buffered stream reader.
     const file = try std.fs.cwd().openFile(binary_file_path, .{});
     defer file.close();
-
-    const buf_reader = std.io.bufferedReader(file.reader());
+    var buf_reader = std.io.bufferedReader(file.reader());
     const in_stream = buf_reader.reader();
 
-    // Read and decode 1 byte at a time
+    // Decode 1 byte at a time.
     var buf: [1]u8 = [_]u8{0};
+    
     try stdout.print("bits 16\n\n", .{});
 
+    // Assume an assembly instruction will never exceed 100 bytes in length.
     var asm_instruction: [100]u8 = [_]u8{0} ** 100;
     while (try in_stream.read(&buf) > 0) {
         try decode(&buf, in_stream, &asm_instruction);
-        try stdout.writeAll(asm_instruction);
+        try stdout.writeAll(&asm_instruction);
         try bw.flush();
     }
 }
